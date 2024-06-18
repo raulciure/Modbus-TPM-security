66931253052f7944a63305041e6239b3d19f3fc3
Raul Ciure <93121896+raulciure@users.noreply.github.com>
2024-06-18 14:38:29 +0300

    Added files required for WolfTPM use

---
+++ TPM/wolftpm/options.h
@@ -0,0 +1,40 @@
+/* wolftpm options.h
+ * generated from configure options
+ *
+ * Copyright (C) 2006-2021 wolfSSL Inc.
+ *
+ * * This file is part of wolfTPM.
+ *
+ */
+
+#ifndef WOLFMTPM_OPTIONS_H
+#define WOLFMTPM_OPTIONS_H
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#undef  HAVE_THREAD_LS
+#define HAVE_THREAD_LS
+
+#undef  HAVE_PTHREAD
+#define HAVE_PTHREAD
+
+#undef  SIZEOF_LONG_LONG
+#define SIZEOF_LONG_LONG 8
+
+#undef  WOLFTPM_SLB9672
+#define WOLFTPM_SLB9672
+
+#undef  WOLFTPM_EXAMPLE_HAL
+#define WOLFTPM_EXAMPLE_HAL
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* WOLFMTPM_OPTIONS_H */
+
